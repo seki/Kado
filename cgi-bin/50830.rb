95a763e068ab82ef75490b74d8e8ca8870daa8ce
Masatoshi SEKI <m_seki@mva.biglobe.ne.jp>
2012-02-26 20:12:12 +0900

    get port number from $0

--- cgi-bin/50830.rb
+++ cgi-bin/50830.rb
@@ -3,5 +3,5 @@
 require 'drb/drb'
 
 DRb.start_service('druby://localhost:0')
-ro = DRbObject.new_with_uri('druby://localhost:50830')
+ro = DRbObject.new_with_uri('druby://localhost:' +  File.basename($0, '.rb'))
 ro.start(ENV.to_hash, $stdin, $stdout)
